56cd67aa132bb47e10c7e6cba012e4e9ff66ae36
Robert Pliszka <robplisz@gmail.com>
2022-05-17 16:57:19 -0400

    Added api routes for get candidate, get candidate by id, and delete candidate

--- server.js
+++ server.js
@@ -51,22 +51,50 @@ app.get('/api/candidates', (req, res) => {
 
 // Get single candidate from db
 
-// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) =>{
-//     if (err) {
-//         console.log(err);
-//     }
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+});
 
-//     console.log(row);
-// });
+
+
+
 
 // Delete a candidate from db
 
-// db.query(`DELETE FROM candidates WHERE id=?`, 1, (err, result) => {
-//     if(err){
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if(err){
+            res.statusMessage(400).json({ error: res.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
+
 
 // Create a cadidate in the db
 
