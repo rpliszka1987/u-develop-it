281e81808184bb3ad71246bd64fb24bfed8d6aa4
Robert Pliszka <robplisz@gmail.com>
2022-05-18 17:33:16 -0400

    Added voters API route to get all and individual voters by id

---
+++ routes/apiRoutes/voterRoutes.js
@@ -0,0 +1,42 @@
+const express = require('Express');
+const router = express.Router();
+const db = require('../../db/connection');
+const inputCheck = require('../../utils/inputCheck');
+
+
+// Get voters
+
+router.get('/voters', (req, res) => {
+    const sql = `SELECT * FROM voters ORDER BY last_name`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+// Get a voter
+
+router.get('/voter/:id', (req, res) => {
+    const sql = `SELECT * FROM voters WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+module.exports = router;
